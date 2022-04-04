6a86cab368df9d1baa83c97180905ddad60e2cf1
Tom Haynes <tomhaynes94@gmail.com>
2022-04-04 15:53:44 +0100

    completed fizzbuzz & boolean kata test exercises

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,21 @@
-const fizzBuzz = number => {};
+const fizzBuzz = (number) => {
+    const isDivisbleBy3 = (number % 3) === 0;
+    const isDivisbleBy5 = (number % 5) === 0;
 
+    if (isDivisbleBy3 && isDivisbleBy5) {
+        return "FizzBuzz";
+    }
+
+    if (isDivisbleBy3) {
+        return "Fizz";
+        }
+    
+    if (isDivisbleBy5) {
+        return "Buzz";
+    }
+
+return number 
+
+};
+    
 module.exports = fizzBuzz;
--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,8 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it("returns 'yes' if boolean value is true and returns 'no' if boolean value is false", () => {
+    expect(booleanToWord(true)).toBe("Yes");
+    expect(booleanToWord(false)).toBe("No");
+  });
 });
